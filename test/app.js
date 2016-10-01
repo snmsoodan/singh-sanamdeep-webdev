3cbf069d23e3773b469f3b7953d5df0852d642c5
Sanamdeep Singh <snm91@ccs.neu.edu>
2016-10-01 00:40:40 -0400

    user and page complete

--- test/app.js
+++ test/app.js
@@ -4,7 +4,8 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    //var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString ='mongodb://sanamsoodan:harman587@ds033066.mlab.com:33066/singh-sanamdeep';
     //var connectionString = 'mongodb://sanamsoodan:harman587@cluster0-shard-00-00-sy52b.mongodb.net:27017,cluster0-shard-00-01-sy52b.mongodb.net:27017,cluster0-shard-00-02-sy52b.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
     // if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     //     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
